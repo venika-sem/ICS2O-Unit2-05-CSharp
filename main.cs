7c4238f66dd415892427093e6947f7f5ab31ac78
venika-sem <99354917+venika-sem@users.noreply.github.com>
2022-03-27 18:28:17 +0000

    changed main.cs

--- main.cs
+++ main.cs
@@ -9,18 +9,16 @@ class Program
 {
     public static void Main(string[] args)
     {
-       // This function calculates the pay and government tax of the user
+        // This function calculates the pay and government tax of the user
         float numberOfHours;
         float hourlyPay;
 
         Console.WriteLine("Please enter your employment information:");
         Console.WriteLine("");
-      
         Console.WriteLine("Number of hours worked: ");
         numberOfHours = Convert.ToSingle(Console.ReadLine());
         Console.WriteLine("Hourly rate: ");
         hourlyPay = Convert.ToSingle(Console.ReadLine());
-      
         Console.WriteLine("");
         Console.WriteLine("Your pay will be $" + ((numberOfHours * hourlyPay) * (1 - 0.18)).ToString("0.00") + ", and the government will take $" + ((numberOfHours * hourlyPay) * 0.18).ToString("0.00") + ".");
 
