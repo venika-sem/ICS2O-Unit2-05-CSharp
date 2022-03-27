c037b5ca371fa5f168ecde220a7c997d61c834ff
venika-sem <99354917+venika-sem@users.noreply.github.com>
2022-03-27 18:21:37 +0000

    changed main.cs

--- main.cs
+++ main.cs
@@ -1,7 +1,29 @@
+// Created by: Venika Sem
+// Created on: Mar 2022
+//
+// This program does calculations
+
 using System;
 
-class Program {
-  public static void Main (string[] args) {
-    Console.WriteLine ("Hello World");
-  }
+class Program
+{
+    public static void Main(string[] args)
+    {
+       // This function calculates the pay and government tax of the user
+        float numberOfHours;
+        float hourlyPay;
+
+        Console.WriteLine("Please enter your employment information:");
+        Console.WriteLine("");
+      
+        Console.WriteLine("Number of hours worked: ");
+        numberOfHours = Convert.ToSingle(Console.ReadLine());
+        Console.WriteLine("Hourly rate: ");
+        hourlyPay = Convert.ToSingle(Console.ReadLine());
+      
+        Console.WriteLine("");
+        Console.WriteLine("Your pay will be $" + ((numberOfHours * hourlyPay) * (1 - 0.18)).ToString("0.00") + ", and the government will take $" + ((numberOfHours * hourlyPay) * 0.18).ToString("0.00") + ".");
+
+        Console.WriteLine("\nDone.");
+    }
 }
